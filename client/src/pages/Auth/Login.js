bb05b16b610d722f5f00ed6d53eb87654bfb83bb
shrabanti1 <jharnamazumdar78623@gmail.com>
2023-08-14 20:43:06 +0530

    Update Login.js

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:8080/api/v1/auth/login", {
+      const res = await axios.post("https://ecommerce-backend-s1yc.onrender.com/api/v1/auth/login", {
         email,
         password,
       });

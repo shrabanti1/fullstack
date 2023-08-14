07a5b38c6a79fa5f064bd774d7888daf2665125e
shrabanti1 <jharnamazumdar78623@gmail.com>
2023-08-15 02:37:11 +0530

    Update Login.js

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:8080/api/v1/auth/login",
+        "http://localhost:10000/api/v1/auth/login",
         {
           email,
           password,

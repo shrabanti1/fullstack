234849787ac2324980af10f51ec39b37fad27a0e
shrabanti1 <jharnamazumdar78623@gmail.com>
2023-08-15 01:03:34 +0530

    Update useCategory.js

--- client/src/hooks/useCategory.js
+++ client/src/hooks/useCategory.js
@@ -8,7 +8,7 @@ export default function useCategory() {
   const getCategories = async () => {
     try {
       const { data } = await axios.get(
-        "https://ecommerce-backend-s1yc.onrender.com/api/v1/category/get-category"
+        "http://localhost:8080/api/v1/category/get-category"
       );
       setCategories(data?.category);
     } catch (error) {

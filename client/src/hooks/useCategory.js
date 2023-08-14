7457e376e2fdf697f7227d0cb73d346ddfe0c36b
shrabanti1 <jharnamazumdar78623@gmail.com>
2023-08-15 02:42:11 +0530

    Update useCategory.js

--- client/src/hooks/useCategory.js
+++ client/src/hooks/useCategory.js
@@ -8,7 +8,7 @@ export default function useCategory() {
   const getCategories = async () => {
     try {
       const { data } = await axios.get(
-        "http://localhost:8080/api/v1/category/get-category"
+        "http://localhost:10000/api/v1/category/get-category"
       );
       setCategories(data?.category);
     } catch (error) {

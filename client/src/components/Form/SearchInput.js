1613bd5aac654e3e62e4e3695e35b5ec52496e6a
shrabanti1 <jharnamazumdar78623@gmail.com>
2023-08-15 02:43:30 +0530

    Update SearchInput.js

--- client/src/components/Form/SearchInput.js
+++ client/src/components/Form/SearchInput.js
@@ -10,7 +10,7 @@ const SearchInput = () => {
     e.preventDefault();
     try {
       const { data } = await axios.get(
-        `http://localhost:8080/api/v1/product/search/${values.keyword}`
+        `http://localhost:10000/api/v1/product/search/${values.keyword}`
       );
       setValues({ ...values, results: data });
       navigate("/search");
